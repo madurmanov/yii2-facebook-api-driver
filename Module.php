6f6dfdfe403fb87c6d9e932aad903c6e9ebf69c0
Mikhail Durmanov <madurmanov@gmail.com>
2017-11-18 20:49:54 +0300

    Remove access token part

--- Module.php
+++ Module.php
@@ -7,18 +7,14 @@ use yii\base\Module as BaseModule;
 class Module extends BaseModule
 {
   const API_URL = 'https://graph.facebook.com';
-  const OAUTH_PERMISSION_URL = 'https://www.facebook.com/dialog/oauth';
-  const OAUTH_ACCESS_TOKEN_URL = 'https://graph.facebook.com/oauth/access_token';
 
-  public $clientID = 0;
   public $pageID = 0;
-  public $secret = '';
   public $scope = [];
   public $accessToken = '';
 
   public function request($method, $type = 'GET', $params = [], $accessToken = false)
   {
-    $result = '';
+    $result = false;
     if ($accessToken) $params['access_token'] = $this->accessToken;
     $url = self::API_URL . "/{$this->pageID}/{$method}";
     switch ($type) {
@@ -37,27 +33,5 @@ class Module extends BaseModule
         break;
     }
     return json_decode($result);
-  }
-
-  public function getAccessTokenUrl()
-  {
-    return self::OAUTH_ACCESS_TOKEN_URL
-      . '?' . http_build_query([
-        'type' => 'client_cred',
-        'client_id' => $this->clientID,
-        'client_secret' => $this->secret
-      ]);
-  }
-
-  public function getPermissionUrl($callback)
-  {
-    return self::OAUTH_PERMISSION_URL
-      . '?' . http_build_query([
-        'client_id' => $this->clientID,
-        'client_secret' => $this->secret,
-        'redirect_uri' => $callback,
-        'scope' => implode(',', $this->scope),
-        'response_type' => 'token'
-      ]);
   }
 }
