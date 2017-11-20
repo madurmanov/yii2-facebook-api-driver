840ba9423e0762fb05c9a6748229089c57a49753
Mikhail Durmanov <madurmanov@gmail.com>
2017-11-20 17:33:27 +0300

    Adapt module for facebook-graph-sdk and update README.md

--- Module.php
+++ Module.php
@@ -2,35 +2,56 @@
 
 namespace madurmanov\FacebookApiDriver;
 
+use Facebook\Facebook as FacebookSDK;
 use yii\base\Module as BaseModule;
 
 class Module extends BaseModule
 {
-  const API_URL = 'https://graph.facebook.com';
-
+  public $appID = 0;
+  public $appSecret = '';
   public $pageID = 0;
   public $accessToken = '';
+  public $graphVersion = 'v2.8';
 
-  public function request($method, $type = 'GET', $params = [], $accessToken = false)
+  protected $api = null;
+
+  public function init()
   {
-    $result = false;
-    if ($accessToken) $params['access_token'] = $this->accessToken;
-    $url = self::API_URL . "/{$this->pageID}/{$method}";
-    switch ($type) {
-      case 'GET':
-        $url .= "?" . http_build_query($params);
-        $result = file_get_contents($url);
-        break;
-      case 'POST':
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        break;
+    $this->api = new FacebookSDK([
+      'app_id' => $this->appID,
+      'app_secret' => $this->appSecret,
+      'default_graph_version' => $this->graphVersion
+    ]);
+    if ($this->accessToken) {
+      $this->api->setDefaultAccessToken($this->accessToken);
     }
-    return json_decode($result);
+  }
+
+  public function request($method, $params = [], $accessToken = '')
+  {
+    if (!$accessToken) $accessToken = $this->accessToken;
+    return $this->api->post($method, $params, $accessToken);
+  }
+
+  public function getPageAccessToken($pageID)
+  {
+    $pages = $this->api->get('/me/accounts');
+    $pages = $pages->getGraphEdge()->asArray();
+    foreach ($pages as $page) {
+      if ($page['id'] == $pageID) return $page['access_token'];
+    }
+    return null;
+  }
+
+  public function login($redirectUri, $permissions)
+  {
+    $helper = $this->api->getRedirectLoginHelper();
+    $accessToken = $helper->getAccessToken();
+    if (isset($accessToken)) {
+      $oAuth2Client = $this->api->getOAuth2Client();
+      $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+      return (string) $longLivedAccessToken;
+    }
+    return $helper->getLoginUrl($redirectUri, $permissions);
   }
 }
--- README.md
+++ README.md
@@ -6,11 +6,25 @@ Module that enables send request to facebook api for Yii Framework 2.0.
 composer require madurmanov/yii2-facebook-api-driver "@dev"
 ```
 
+## AccessToken
+- Create app [https://developers.facebook.com](https://developers.facebook.com)
+- Set enabled url addresses in app settings and product login settings
+- Execute next code with entered `appID` and `appSecret` parameters in module configuration
+```
+$FacebookApiDriver = Yii::$app->getModule('FacebookApiDriver');
+// link should be coincide with page url where function call
+var_dump($FacebookApiDriver->login('http://example.com', ['manage_pages', 'publish_pages']));
+```
+- First you will get link for login on which you need to follow
+- After redirect to entered page you will get access token for configurate
+
 ## Configuration
 ```
 'modules' => [
   'FacebookApiDriver' => [
     'class' => 'madurmanov\FacebookApiDriver\Module',
+    'appID' => 0,
+    'appSecret' => 0,
     'pageID' => 0,
     'accessToken' => ''
   ]
@@ -20,9 +34,9 @@ composer require madurmanov/yii2-facebook-api-driver "@dev"
 ## Usage
 ```
 $FacebookApiDriver = Yii::$app->getModule('FacebookApiDriver');
-$FacebookApiDriver->request('feed', 'POST', [
+$FacebookApiDriver->request("/{$FacebookApiDriver->pageID}/feed", 'POST', [
   'message' => ''
-], true);
+], $FacebookApiDriver->getPageAccessToken($FacebookApiDriver->pageID));
 ```
 
 ## License
